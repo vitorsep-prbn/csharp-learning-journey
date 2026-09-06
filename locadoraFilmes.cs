06cccc286693ebe4c8d4a3d6736cce66d28d0cc9
vitorsep-prbn <vivitorinoarts@gmail.com>
2026-09-06 20:47:10 -0300

    Refactor locadoraFilmes class with improved UI

--- locadoraFilmes.cs
+++ locadoraFilmes.cs
@@ -1,21 +1,26 @@
-using System.Security.AccessControl;
-
 public class locadoradeFilmes
 {
     public static void Executar()
     {
+
+
         Console.Clear();
-        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("-- LOCADORA IDHEM --");
+        Console.ResetColor();
         Thread.Sleep(3000);
         Console.Clear();
 
+        Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("-LOGIN- ");
         Console.WriteLine("");
+
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("Digite seu usuário:");
         string usuario = Console.ReadLine();
 
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("Digite a senha:");
         string senha = Console.ReadLine();
 
@@ -38,86 +43,107 @@ public class locadoradeFilmes
         while (opcao != 4)
         {
             Console.Clear();
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("1 - ALUGAR FILMES");
             Console.WriteLine("2 - MEUS FILMES");
             Console.WriteLine("3 - DEVOLVER FILMES");
             Console.WriteLine("4 - SAIR");
 
+            Console.ForegroundColor = ConsoleColor.Yellow;
             opcao = int.Parse(Console.ReadLine());
 
             if (opcao == 1)
             {
                 Thread.Sleep(1000);
                 Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("ALUGAR FILMES:");
                 Console.WriteLine("");
+
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("Qual filme deseja alugar? (digite apenas o número)");
+
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(" 1 - Vingadores: Ultimato - R$8 a diária");
+
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine(" 2 - Homem-Aranha: Um novo dia - R$15 a diária");
+
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine(" 3 - Gente Grande 3 - R$5 a diária");
+
                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine(" 4 - O poderoso chefão - R$10 a diária");
 
+                Console.ResetColor();
                 int filme = int.Parse(Console.ReadLine());
 
                 Console.Clear();
                 if (filme == 1)
                 {
                     meusFilmes = "Vingadores: Ultimato";
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Parabéns, filme alugado com sucesso. Para acessá-lo (em breve)");
                 }
                 else if (filme == 2)
                 {
                     meusFilmes = "Homem-Aranha: Um novo dia";
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Parabéns, filme alugado com sucesso. Para acessá-lo (em breve)");
                 }
                 else if (filme == 3)
                 {
                     meusFilmes = "Vingadores: Ultimato";
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Parabéns, filme alugado com sucesso. Para acessá-lo (em breve)");
                 }
                 else if (filme == 4)
                 {
                     meusFilmes = "O poderoso chefão";
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("Parabéns, filme alugado com sucesso. Para acessá-lo (em breve)");
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Filme inválido, tente novamente");
                 }
 
+                Console.ResetColor();
                 Console.ReadLine();
             }
             else if (opcao == 2)
             {
                 Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Cyan;
                 Thread.Sleep(1000);
                 Console.WriteLine("Meus Filmes");
+
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine(meusFilmes);
 
                 Console.ReadLine();
+                Console.ResetColor();
             }
-
-
             else if (opcao == 3)
             {
                 Console.Clear();
                 if (meusFilmes == "")
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.WriteLine("Não tem filmes para devolver.");
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("FILMES ALUGADOS:");
+
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine(meusFilmes);
 
-                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
                     Console.WriteLine("Deseja devolver o filme? s/n?");
                     string devolver = Console.ReadLine();
 
@@ -138,13 +164,11 @@ public class locadoradeFilmes
                     }
                 }
 
-
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("Pressione ENTER para voltar ao menu...");
                 Console.ReadLine();
             }
-
-
         }
+
     }
 }
